71fa751e6c7e942ae80c04f9d8c6b6ba03323aa1
chriskim0708 <last1224@gmail.com>
2023-03-05 15:37:27 +0900

    study 03-05

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState({
+    box1: "red",
+    box2: "blue",
+    box3: "green",
+  });
+  const boxStyle = {
+    width: "200px",
+    height: "200px",
+  };
+  const onChangeColor = () => {
+    setColor((state) => {
+      return {
+        ...state,
+        box2: "green",
+        box3: "blue",
+      };
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <button onClick={onChangeColor}>change color</button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+        }}
+      >
+        <div
+          style={{
+            ...boxStyle,
+            backgroundColor: color.box1,
+          }}
+        ></div>
+        <div
+          style={{
+            ...boxStyle,
+            backgroundColor: color.box2,
+          }}
+        ></div>
+        <div
+          style={{
+            ...boxStyle,
+            backgroundColor: color.box3,
+          }}
+        ></div>
+      </div>
+    </>
   );
 }
 
